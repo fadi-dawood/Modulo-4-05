1309ae90edf41ca0626c2ed74dd1b80d5f0f3ec8
Fadi_Dawood <dawood_fadi@hotmail.com>
2024-02-14 21:28:26 +0100

    finito

--- detail-page/detail.js
+++ detail-page/detail.js
@@ -10,25 +10,8 @@ fetch("https://striveschool-api.herokuapp.com/books/"+id)
     .catch(err => console.log(err))
 
 function creatDetailPage(bookDetail) {
-    //     <div class="card mb-3 cart-item">
-    // <div class="row g-0">
-    //     <div class="col-md-4">
-    //         <img src="..." class="img-fluid rounded-start" alt="...">
-    //     </div>
-    //     <div class="col-md-8">
-    //         <div class="card-body">
-    //             <h5 class="card-title">Card title</h5>
-    //             <p class="card-text">This is a wider card with supporting text below as a
-    //                 natural lead-in to additional content. This content is a little bit
-    //                 longer.
-    //             </p>
-    //         </div>
-    //     </div>
-    // </div>
-    // </div>
-
     let bookBox = document.createElement("div");
-    bookBox.classList.add("card", "mb-3", "cart-item", "p-3", "mb-5", "border-0");
+    bookBox.classList.add("card", "mb-3", "cart-item", "p-3", "m-5", "border-0");
     bookBox.innerHTML =
         `<div class="row g-0">
     <div class="col-md-4">
@@ -41,7 +24,6 @@ function creatDetailPage(bookDetail) {
             </p>
             <p class="card-text my-2">Price: ${bookDetail.price}
             </p>
-            <button type="button" class="fw-bolder btn btn-dark add-to-cart-btn my-2">Add to Cart</button>
         </div>
     </div>
 </div>`
--- search-page/script.js
+++ search-page/script.js
@@ -1,26 +1,38 @@
-// endpoint: https://striveschool-api.herokuapp.com/books
-const endPoint ="https://striveschool-api.herokuapp.com/books";
+//& endpoint: https://striveschool-api.herokuapp.com/books
+const endPoint = "https://striveschool-api.herokuapp.com/books";
 
+// Number of the items inside the cart: 
+let cartItems = 0;
 
+//& chiamata HTTP
 fetch("https://striveschool-api.herokuapp.com/books")
-    .then(response => response.json())
-    .then(json => {
-        caricaLibri(json);
-        search(json, caricaLibri);
-    })
-    .catch(err => console.log(err));
+.then(response => response.json())
+.then(json => {
+    caricaLibri(json);
+    search(json, caricaLibri);
+})
+.catch(err => console.log(err));
 
+
+
+//& chiamare containers dal DOM
+// search's result container
 let bookContainer = document.getElementById("books-container");
+
+// Cart container
 let cartContainer = document.getElementById("cart-items");
-let cartItems = [];
 
 
+
+//& carica i prodotti
 function caricaLibri(booksArray) {
     const allBooks = [...booksArray];
     allBooks.forEach((element) => {
+
         // Creare un nuovo card:
         let newBookCard = document.createElement("div");
         newBookCard.classList.add("card", "book", "shadow-sm", "p-3", "mb-4", "bg-body-tertiary", "rounded", "mx-2");
+        newBookCard.id = element.asin;
         newBookCard.style.width = "18rem";
         bookContainer.appendChild(newBookCard);
 
@@ -67,14 +79,20 @@ function caricaLibri(booksArray) {
 
         // add to the cart:
         cartBtn.addEventListener("click", () => {
-            cartItems.push(element);
+            newBookCard.classList.toggle("added-to-cart");
             addToCart(element);
+        });
+
+        // Skip this card:
+        skipBtn.addEventListener("click", () => {
+            skipBtn.parentNode.parentNode.classList.add("d-none");
         });
     });
 };
 
 
-//* Search function:
+
+//& Search function:
 let searchBtn = document.getElementById("search-btn");
 
 function search(booksArray, callback) {
@@ -95,16 +113,20 @@ function search(booksArray, callback) {
         //show filtered cards: 
         callback(searchResualt)
     });
-
-
 };
 
 
 
-// add product to the cart:
+//& add product to the cart:
 function addToCart(element) {
+    // aggiornare il numero dei prodotti nel carello
+    cartItems ++;
+    console.log(cartItems);
+
+    // mostrare il prodotto aggiunto
     let cartItemContainer = document.createElement("div");
     cartItemContainer.classList.add("card", "mb-3", "cart-item");
+    cartItemContainer.id = "card-" + element.asin;
     cartItemContainer.style.maxWidth = "540px";
     cartItemContainer.innerHTML = `
         <div class="row g-0">
@@ -122,21 +144,48 @@ function addToCart(element) {
         </div>`
 
     cartContainer.appendChild(cartItemContainer);
+
     // attivare il tasto: cancella dal carello
     deleteItemCart();
+
+    // numero items dentro il carello:
+    document.getElementById("NumOfItemsCart").innerText = cartItems;
 };
 
 
-function deleteItemCart(){
+
+//& Empty your cart:
+document.getElementById("empty-cart").addEventListener("click", () => {
+    document.querySelectorAll(".cart-item").forEach(element => {
+        element.remove();
+    });
+    // numero items dentro il carello:
+    cartItems = 0;
+    document.getElementById("NumOfItemsCart").innerText = cartItems;
+});
+
+
+
+
+//& Delete all elemento of cart
+function deleteItemCart() {
     document.querySelectorAll(".delete-from-cart-btn").forEach((btn) => {
         btn.addEventListener("click", (event) => {
+
             // Seleziona l'indice del pulsante "Delete" cliccato
             let index = Array.from(document.querySelectorAll(".delete-from-cart-btn")).indexOf(event.target);
+
             // Rimuovi l'elemento corrispondente nell'elenco degli elementi del carrello
             document.querySelectorAll(".cart-item")[index].remove();
+
+            // cancella lo stile:
+            let cartId = event.target.parentNode.parentNode.parentNode.getAttribute("id").substring(5);
+            document.getElementById(cartId).classList.toggle("added-to-cart");
+
+            // aggiornare il numero dei prodotti nel carello
+            cartItems = cartItems - 1;
+            document.getElementById("NumOfItemsCart").innerText = cartItems;
         });
     });
-    
-    
-}
+};
 
